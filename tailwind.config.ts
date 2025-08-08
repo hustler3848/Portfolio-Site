af23b2e0275ec4c2a8b4f2ca718d0818cd56b30c
hustler hustler <hustlerh3848@gmail.com>
2025-08-08 09:55:20 +0000

    I think we can use this font Familjen Grotesk for paragraph texts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Bricolage Grotesque"', 'sans-serif'],
+        body: ['"Familjen Grotesk"', 'sans-serif'],
         headline: ['"Bricolage Grotesque"', 'serif'],
         code: ['monospace'],
       },
