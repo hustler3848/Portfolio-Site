3e259930b4c95cfb2dbef111349addeb9f1143d3
hustler hustler <hustlerh3848@gmail.com>
2025-08-08 12:37:38 +0000

    i am now dploying this into vercel is everything ready ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu3uTiG1PO7JeLMLCg552Uw6H5xRGUINU",
-  authDomain: "darshan-97205.firebaseapp.com",
-  projectId: "darshan-97205",
-  storageBucket: "darshan-97205.firebasestorage.app",
-  messagingSenderId: "244389011898",
-  appId: "1:244389011898:web:1ff2a30824e511292dfd83",
-  measurementId: ""
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
